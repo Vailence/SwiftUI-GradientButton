b7204ecf06695cf05b8829c55f6d39f633aebacf
Akylbek <akke.utekeshev@gmail.com>
2021-05-17 10:09:54 +0600

    Initial Commit

---
+++ GradientButton/GradientButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientButtonApp.swift
+//  GradientButton
+//
+//  Created by Akylbek Utekeshev on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct GradientButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
